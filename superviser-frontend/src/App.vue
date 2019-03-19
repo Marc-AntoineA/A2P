5dae7686e4b90c78fb8ebaf6543e20b9de476f3f
Marc-AntoineA <marc-antoine.auge@ponts.org>
2019-03-19 17:47:38 +0200

    Small modifications: bug while trying to include custom components

--- superviser-frontend/src/App.vue
+++ superviser-frontend/src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <div id="app">
-    <header class="header">
-      <nav class="inner">
-        <router-link to='/' exact>
-          <img class='logo' src='./assets/images/logo.jpg'/>
-        </router-link>
-      </nav>
-    </header>
+    <el-container>
+      <aap-header></aap-header>
+      <el-container>
+        <el-aside>
+          <el-menu>
+            <el-menu-item>Campaign Overview</el-menu-item>
+            <el-menu-item>Edit Mail Templates</el-menu-item>
+            <el-menu-item>Close this Campaign</el-menu-item>
+          </el-menu>
+        </el-aside>
+        <el-main>
+          <h2> Main </h2>
+        </el-main>
+      </el-container>
+      </el-container>
+      <el-footer>
+        FOOTER
+        <el-button>Hello</el-button>
+      </el-footer>
+    </el-container>
   </div>
 </template>
-s
+
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -19,4 +32,9 @@ s
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.logo {
+  height: 90%;
+}
+
 </style>
--- superviser-frontend/src/main.js
+++ superviser-frontend/src/main.js
@@ -1,8 +1,33 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import './plugins/loadElement';
+import { sync } from 'vuex-router-sync';
+import { createStore } from './store';
+import { createRouter } from './router';
+import './plugins/element.js'
 
-Vue.config.productionTip = false
+// TODO remove
+import AapHeader from './components/common/AapHeader.vue';
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+Vue.use(AapHeader);
+Vue.use('ElementUI');
+
+export function createApp() {
+  const router = createRouter();
+  const store = createStore();
+  sync(store, router);
+
+  const app = new Vue({
+    router,
+    store,
+    render: h => h(App)
+  });
+
+  return { app, router, store };
+}
+
+// TODO. see how entry-client.js works
+const { app, router, store } = createApp();
+app.$mount('#app');
