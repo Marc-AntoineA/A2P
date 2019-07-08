b805759404d2a7326002fd8a1ba31d39096fbb97
Marc-Antoine Augé <marc-antoine.auge@eleves.enpc.fr>
2019-07-09 00:51:53 +0200

    Fixing backend/app.js

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,31 @@ app.use(cors({
 }));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+if (typeof(PhusionPassenger) === 'undefined') {
+
+  const mongoose = require('mongoose');
+  const applicantRoutes = require('./routes/applicant');
+  const superviserRoutes = require('./routes/superviser');
+
+
+  let MONGODB_URL = 'mongodb+srv://' + settings.DB_USERNAME + ':';
+  MONGODB_URL += settings.DB_PASSWORD + '@';
+  MONGODB_URL += settings.DB_HOST + '/';
+  MONGODB_URL += settings.DB_NAME + '?retryWrites=true';
+
+  mongoose.connect(MONGODB_URL, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Successfully connected to MongoDB!');
+    app.use('/applicant-api/', applicantRoutes);
+    app.use('/superviser-api/', superviserRoutes);
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB!');
+    console.error(error)
+  });
+}
+
 app.use('/administration/static-superviser/', express.static(path.join(__dirname, '../superviser-frontend/dist/static-superviser')));
 app.get('/administration*', function(req, res) {
   res.sendFile(path.join(__dirname, '../superviser-frontend/dist', 'index.html'));
@@ -28,31 +53,5 @@ app.get('/*', function(req, res, next) {
   }
   res.sendFile(path.join(__dirname, '../applicant-frontend/build', 'index.html'));
 });
-
-if (typeof(PhusionPassenger) === undefined) {
-
-  const mongoose = require('mongoose');
-  const applicantRoutes = require('./routes/applicant');
-  const superviserRoutes = require('./routes/superviser');
-
-
-  let MONGODB_URL = 'mongodb+srv://' + settings.DB_USERNAME + ':';
-  MONGODB_URL += settings.DB_PASSWORD + '@';
-  MONGODB_URL += settings.DB_HOST + '/';
-  MONGODB_URL += settings.DB_NAME + '?retryWrites=true';
-
-  mongoose.connect(MONGODB_URL, { useNewUrlParser: true })
-    .then(() => {
-      console.log('Successfully connected to MongoDB!');
-
-      app.use('/applicant-api/', applicantRoutes);
-      app.use('/superviser-api/', superviserRoutes);
-
-    })
-    .catch((error) => {
-      console.log('Unable to connect to MongoDB!');
-      console.error(error)
-    });
-  }
 
 module.exports = app;
